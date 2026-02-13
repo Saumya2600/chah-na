6ee77a4cdaf14e861dae5946737320553ea11200
Saumya Shah <sm351623@dal.ca>
2026-02-13 16:21:23 -0400

    added new animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,51 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/heart.png';
 import '../styles/Navbar.css';
 
 function Navbar() {
+  const [showLoveMsg, setShowLoveMsg] = useState(false);
+
+  const handleHeartClick = () => {
+    setShowLoveMsg(true);
+  };
+
+  const handleClose = () => {
+    setShowLoveMsg(false);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-logo">
-          <img src={logo} alt="Love Logo" />
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-logo" style={{ cursor: 'pointer' }} onClick={handleHeartClick}>
+            <img src={logo} alt="Love Logo" />
+          </div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/comparison">Comparison Game</Link>
+            </li>
+            <li>
+              <Link to="/promise">My Promise</Link>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/comparison">Comparison Game</Link>
-          </li>
-          <li>
-            <Link to="/promise">My Promise</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      </nav>
+      {showLoveMsg && (
+        <div className="love-heart-overlay">
+          <div className="love-heart-shape">
+            <button className="love-heart-close" onClick={handleClose}>&times;</button>
+            <div className="love-heart-content">
+              I love you so much <span style={{ color: '#ff69b4', fontWeight: 700 }}>Chahna</span>,<br />
+              please forgive me
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
